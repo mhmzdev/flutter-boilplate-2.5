60acdf727c41584a1cca0ac2b08ee31060380908
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2021-10-23 13:30:51 +0500

    build

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,7 +16,7 @@ void main() {
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('01'), findsOneWidget);
+    expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
